d035caf438927462c4d21e1bc82203ead3b0012c
Alhagie Jaiteh <jaiteh@purdue.edu>
2020-04-25 16:06:05 -0400

    I have change the if/else statements to switch cases

--- src/Test1.java
+++ src/Test1.java
@@ -1,31 +1,34 @@
 import java.util.*;
-enum Operation
-{
+
+
+enum Operation {
     ADD, SUBTRACT, MULTIPLY, DIVIDE
 }
+
 public class Test1 {
 
-    public static double basicCalculator(Operation operation, int num1, int num2){
+    public static double basicCalculator(Operation operation, int num1, int num2) {
         double result = 0;
+        switch (operation) {
+            case ADD:
+                result = num1 + num2;
+                break;
+            case SUBTRACT:
+                result = num2 - num1;
+                break;
+            case MULTIPLY:
+                result = num1 * num2;
+                break;
+            case DIVIDE:
+                if (num2 == 0)
+                    System.out.println("Error: u suck");
 
-        if(operation != Operation.ADD) {
-             System.out.println("CS193 sucks");
-        }
-        else if(operation != Operation.SUBTRACT) {
-             System.out.println("CS193 is not everyone's favorite class");
-        }
-        else if(operation != Operation.MULTIPLY) {
-            System.out.println("The CS193 lecturers do not want us to thrive in CS");
-        }
-        else {
-            if(num2 == 0) {
-               System.out.println("Error: u suck");
-            }
-            else {
-                result = num1 / num2;
-            }
-        }
+                else {
+                    result = num1 / num2;
+                }
+                break;
 
+        }
         return result;
     }
 
